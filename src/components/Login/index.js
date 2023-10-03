50d6e703b4eec6475d5b8b0c0f8827c690e8000e
ahmadou5 <ahmadlasauwal@gmail.com>
2023-10-03 13:42:37 +0300

    'login1'

---
+++ src/components/Login/index.js
@@ -0,0 +1,8 @@
+'use client'
+export const LoginCard = () => {
+    return(
+    <div className="w-[87%] h-full bg-green-400" >
+        <p>hey card</p>
+    </div>
+    )
+}
---
+++ src/pages/login/index.js
@@ -0,0 +1,10 @@
+'use client'
+import { LoginCard } from "@/components/Login"
+
+export default function Login () {
+    return(
+    <div className="w-[100%] bg-gray-100 py-6 px-4 h-[700px] text-center lg:hidden">
+        <LoginCard />
+    </div>
+    )
+}

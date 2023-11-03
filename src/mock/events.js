941579f2f84a4cf6a76db7cd120d2e97924bd60e
ahmadou5 <ahmadlasauwal@gmail.com>
2023-11-03 16:29:38 +0300

    'UI-Update'

--- src/mock/events.js
+++ src/mock/events.js
@@ -32,4 +32,59 @@ export const Events = [
         Fee: 'Free',
         src: './google'
     },
+    {
+        name: 'Google Student',
+        startDate: 'Sunday 29/11/2023',
+        stopDate: 'Sunday 29/11/2023',
+        startTime: '9:00 AM',
+        stopTime: '6:00 PM',
+        Descr: 'Google Developers Festival is a yearly Developers Event that helds in kano state Every Year, ',
+        location: 'Remote',
+        Fee: 'Free',
+        src: './google'
+    },
+    {
+        name: 'Google Student',
+        startDate: 'Sunday 29/11/2023',
+        stopDate: 'Sunday 29/11/2023',
+        startTime: '9:00 AM',
+        stopTime: '6:00 PM',
+        Descr: 'Google Developers Festival is a yearly Developers Event that helds in kano state Every Year, ',
+        location: 'Remote',
+        Fee: 'Free',
+        src: './google'
+    },
+    {
+        name: 'Google Student',
+        startDate: 'Sunday 29/11/2023',
+        stopDate: 'Sunday 29/11/2023',
+        startTime: '9:00 AM',
+        stopTime: '6:00 PM',
+        Descr: 'Google Developers Festival is a yearly Developers Event that helds in kano state Every Year, ',
+        location: 'Remote',
+        Fee: 'Free',
+        src: './google'
+    },
+    {
+        name: 'Google Student',
+        startDate: 'Sunday 29/11/2023',
+        stopDate: 'Sunday 29/11/2023',
+        startTime: '9:00 AM',
+        stopTime: '6:00 PM',
+        Descr: 'Google Developers Festival is a yearly Developers Event that helds in kano state Every Year, ',
+        location: 'Remote',
+        Fee: 'Free',
+        src: './google'
+    },
+    {
+        name: 'Google Student',
+        startDate: 'Sunday 29/11/2023',
+        stopDate: 'Sunday 29/11/2023',
+        startTime: '9:00 AM',
+        stopTime: '6:00 PM',
+        Descr: 'Google Developers Festival is a yearly Developers Event that helds in kano state Every Year, ',
+        location: 'Remote',
+        Fee: 'Free',
+        src: './google'
+    },
 ]
